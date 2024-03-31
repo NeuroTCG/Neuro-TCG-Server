8a639fce63419269807710f8c351eaa6f31637f5
KaiDaLuck <kaidaluck@gmail.com>
2024-03-31 18:33:23 +0200

    Renamed into test-client for clarity and changed port to be the same as in main.kt

--- src/commonMain/kotlin/test-client.kt
+++ src/commonMain/kotlin/test-client.kt
@@ -4,7 +4,7 @@ import java.net.InetSocketAddress
 import java.net.Socket
 fun main() {
     val socket = Socket()
-    socket.connect(InetSocketAddress("127.0.0.1", 5001), 1000)
+    socket.connect(InetSocketAddress("127.0.0.1", 9933), 1000)
     println("Connection Successful!")
     val dataIn = DataInputStream(socket.inputStream)
     val dataOut = DataOutputStream(socket.outputStream)
