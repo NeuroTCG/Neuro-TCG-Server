493f779fb2e5ec86956d4334e53cb0252b5c3be4
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2024-04-02 18:20:41 +0200

    Remove korge.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,22 @@
-import korlibs.korge.gradle.*
-
 plugins {
-	alias(libs.plugins.korge)
-    `java-library`
+    kotlin("jvm") version "1.9.21"
 }
 
-korge {
-	id = "com.sample.demo"
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
-// To enable all targets at once
-
-	//targetAll()
-
-// To enable targets based on properties/environment variables
-	//targetDefault()
-
-// To selectively enable targets
-	
-	targetJvm()
-	//targetJs()
-    //targetWasm()
-	//targetDesktop()
-	//targetIos()
-	//targetAndroid // Do not enable Android. Incompatible with Java plugin.
-
-	serializationJson()
+repositories {
+    mavenCentral()
 }
 
 dependencies {
-    add("commonMainApi", project(":deps"))
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-websockets-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-netty:3.2.9")
-    implementation("io.ktor:ktor-websockets:3.2.9")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8")
-    //add("commonMainApi", project(":korge-dragonbones"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
-configureAutoVersions()
-
-allprojects { repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal() } }
-
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,5 @@
-pluginManagement {
-    repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal(); maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") } }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-
-buildscript {
-    val libsTomlFile = File(this.sourceFile?.parentFile, "gradle/libs.versions.toml").readText()
-    var plugins = false
-    var version = ""
-    for (line in libsTomlFile.lines().map { it.trim() }) {
-        if (line.startsWith("#")) continue
-        if (line.startsWith("[plugins]")) plugins = true
-        if (plugins && line.startsWith("korge") && Regex("^korge\\s*=.*").containsMatchIn(line)) version = Regex("version\\s*=\\s*\"(.*?)\"").find(line)?.groupValues?.get(1) ?: error("Can't find korge version")
-    }
-    if (version.isEmpty()) error("Can't find korge version in $libsTomlFile")
-
-    repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal() }
-
-    dependencies {
-        classpath("com.soywiz.korge.settings:com.soywiz.korge.settings.gradle.plugin:$version")
-    }
-}
-
-apply(plugin = "com.soywiz.korge.settings")
-
 rootProject.name = "Neuro-TCG-server"
+
