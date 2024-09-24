e2114f3b0a0a354055efb00819beea85689dba09
Superbox2147 <132390694+Superbox2147@users.noreply.github.com>
2024-09-24 15:51:45 +0300

    change dependencies
    TODO: update ktor further to 2.3.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     application
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 val exposedVersion = "0.49.0"
+val ktorVersion = "1.6.8" //goal: change this to 2.3.12
 
 repositories {
     mavenCentral()
@@ -14,16 +15,18 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.7.1")
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-java-time", exposedVersion)
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
-    implementation("io.ktor:ktor-server-netty:1.6.4")
-    implementation("io.ktor:ktor-websockets:1.6.4")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-websockets:$ktorVersion")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
 
 application {
